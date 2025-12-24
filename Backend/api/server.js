c412ddb2e53d5110a383466524a6cd4c11575652
Abhay kumar singh <abhaykumarsingh2904@gmail.com>
2025-12-24 18:32:19 +0530

    fixing cors error

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://chatifyweb.vercel.com"
+      "https://chatifyweb.vercel.app"
 
     ],
     credentials: true
